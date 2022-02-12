03e50b31fec6fa905eda3998f99767db570a7f76
Alex Craviotto <alejandrogc303@gmail.com>
2022-02-12 16:21:21 +0100

    Script added

---
+++ ResyncTime.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: Force administrator permissions
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+
+:: Fixed common errors with w32tm
+w32tm /unregister
+w32tm /register
+:: Fixed common errors with w32tm
+net start w32time
+w32tm /resync
+
+exit
